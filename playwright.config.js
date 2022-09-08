715b8a8dbd26725eecd4a603896c34387fa4b04f
yilinng <wei9912@gmail.com>
2022-09-08 09:46:55 +0800

    add jenkkinsfile

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ const config = {
 
   use: {
     baseURL: 'http://localhost:3000',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry'
